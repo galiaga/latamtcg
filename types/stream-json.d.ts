be32c305602882ce6d7544c53704629dcfa013ae
Gaston Aliaga <galiaga@gmail.com>
2025-10-21 14:57:10 -0300

    LAT-70: Prices Scryfall update v2

--- types/stream-json.d.ts
+++ types/stream-json.d.ts
@@ -1,3 +1,10 @@
+declare module 'stream-chain' {
+  import { Transform } from 'stream'
+  export function chain(transforms: Transform[]): Transform
+  const _default: { chain: typeof chain }
+  export default _default
+}
+
 declare module 'stream-json' {
   import { Transform } from 'stream'
   export function parser(options?: unknown): Transform
@@ -7,8 +14,8 @@ declare module 'stream-json' {
 
 declare module 'stream-json/streamers/StreamArray' {
   import { Transform } from 'stream'
-  export function withParser(options?: unknown): Transform
-  const _default: { withParser: typeof withParser }
+  export function streamArray(options?: unknown): Transform
+  const _default: { streamArray: typeof streamArray }
   export default _default
 }
 
